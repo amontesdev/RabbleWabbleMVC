b8b7d57d199eccc4bd613fd96271b1d1ceea6656
andres <ingmontes21@gmail.com>
2019-06-11 00:04:16 -0400

    structure of projects and add some item to main view

---
+++ RabbleWabbleMVC/Models/Question.swift
@@ -0,0 +1,15 @@
+//
+//  Question.swift
+//  RabbleWabbleMVC
+//
+//  Created by user156150 on 6/10/19.
+//  Copyright © 2019 Andres Montes. All rights reserved.
+//
+
+import Foundation
+
+public struct Question{
+    public let answer:String?
+    public let hint:String?
+    public let prompt:String?
+}
---
+++ RabbleWabbleMVC/Views/QuestionView.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionView.swift
+//  RabbleWabbleMVC
+//
+//  Created by user156150 on 6/10/19.
+//  Copyright © 2019 Andres Montes. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class QuestionView:UIView{
+    @IBOutlet public var answerLabel:UILabel!
+    @IBOutlet public var correctCountLabel:UILabel!
+    @IBOutlet public var incorrectCountLabel:UILabel!
+    @IBOutlet public var prompt:UILabel!
+    @IBOutlet public var hint:UILabel!
+}
