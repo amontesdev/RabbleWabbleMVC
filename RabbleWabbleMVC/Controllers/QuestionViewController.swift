48e4ca08cf2ee0c9defdbf364eb5bddbcb20127a
andres <ingmontes21@gmail.com>
2019-06-14 00:32:43 -0400

    add some constrainst to main view and add function to show questions

--- RabbleWabbleMVC/Controllers/QuestionViewController.swift
+++ RabbleWabbleMVC/Controllers/QuestionViewController.swift
@@ -3,16 +3,40 @@
 //  RabbleWabbleMVC
 //
 //  Created by user156150 on 6/10/19.
-//  Copyright © 2019 Andres Montes. All rights reserved.
+//  Copyright © 2019 Andres Montes. All rights .
 //
 
 import UIKit
 
 class QuestionViewController: UIViewController {
-
+    
+    //MARK: - Vars
+    public var questionGroup = QuestionGroup.basicPhrases()
+    public var questionIndex = 0
+    public var correctCount = 0
+    public var incorrectCount = 0
+    public var questionView: QuestionView!{
+        guard isViewLoaded else {
+            return nil
+        }
+        return (view as! QuestionView)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        showQuestions()
+    }
+    
+    //MARK: - IBActions and functions
+    private func showQuestions(){
+        
+        let question = questionGroup.questions?[questionIndex]
+        
+        questionView.answerLabel.text = question?.answer
+        questionView.hint.text = question?.hint
+        questionView.prompt.text = question?.prompt
+        
+        questionView.answerLabel.isHidden = true
+        questionView.hint.isHidden = true
     }
 
 
